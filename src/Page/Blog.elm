266c0799c7047a906e250ce78786d3ad9438900d
Harald Ringvold <harald.ringvold@gmail.com>
2022-04-21 23:06:38 +0200

    Add article divider on blog index

--- src/Page/Blog.elm
+++ src/Page/Blog.elm
@@ -134,4 +134,5 @@ viewBlogEntry entry =
             [ Link.link entry.route [] (text entry.info.title) ]
         , Shared.timestampView entry
         , p [] [ text entry.info.description ]
+        , hr [ css [ Tw.m_10 ] ] []
         ]
